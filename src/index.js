24197fc920d51b937c8e940a8ee700b97766ced1
Elly Pirelly <47147981+EllyPirelly@users.noreply.github.com>
2023-04-26 12:43:16 +0200

    fix ReactDom Client not working

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// does not work
+// import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
@@ -11,6 +13,14 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+// does not work
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// )
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
