273cd639f7cbdf956d6cfd7043559136d1cacce6
Elly Pirelly <47147981+EllyPirelly@users.noreply.github.com>
2023-05-03 12:32:48 +0200

    clean-up end-to-end test 1 and 2

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,33 +1,35 @@
 import puppeteer from 'puppeteer';
 
 describe('show/hide an event details', () => {
-  test('An event element is collapsed by default', async () => {
-    const browser = await puppeteer.launch();
+  let browser;
+  let page;
 
-    const page = await browser.newPage();
+  beforeAll(async () => {
+    jest.setTimeout(30000);
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
     await page.goto('http://localhost:3000/');
-
-    // ensure that event list is loaded, waits for Event to be loaded
+    // ensure that event list is loaded, waits for Event to be rendered
     await page.waitForSelector('.event-container');
+  });
 
+  afterAll(() => {
+    browser.close();
+  });
+
+  test('An event element is collapsed by default', async () => {
     // selecting an element on the page; returning the first CSS selector found
     // element is present? expanded
     // element is not present? collapese
     const eventDetails = await page.$('.event-container .event-info-details');
     expect(eventDetails).toBeNull();
-    browser.close();
   });
 
   test('User can expand an event to see its details', async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('http://localhost:3000/');
-
-    await page.waitForSelector('.event-container');
+    // simulate user click on toggle-details button
     await page.click('.event-container .toggle-details');
-
     const eventDetails = await page.$('.event-container .event-info-details');
+    // eventDetails need to exist at this time
     expect(eventDetails).toBeDefined();
-    browser.close();
   });
 });
