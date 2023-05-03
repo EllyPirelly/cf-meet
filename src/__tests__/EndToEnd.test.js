878c6e856c9d172008d78369094c32f627330924
Elly Pirelly <47147981+EllyPirelly@users.noreply.github.com>
2023-05-03 12:20:23 +0200

    fix selector, add second end-to-end test

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -8,13 +8,26 @@ describe('show/hide an event details', () => {
     await page.goto('http://localhost:3000/');
 
     // ensure that event list is loaded, waits for Event to be loaded
-    await page.waitForSelector('.event');
+    await page.waitForSelector('.event-container');
 
     // selecting an element on the page; returning the first CSS selector found
     // element is present? expanded
     // element is not present? collapese
     const eventDetails = await page.$('.event-container .event-info-details');
     expect(eventDetails).toBeNull();
+    browser.close();
+  });
+
+  test('User can expand an event to see its details', async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+
+    await page.waitForSelector('.event-container');
+    await page.click('.event-container .toggle-details');
+
+    const eventDetails = await page.$('.event-container .event-info-details');
+    expect(eventDetails).toBeDefined();
     browser.close();
   });
 });
