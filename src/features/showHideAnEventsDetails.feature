2bb66dc502bbb93ca56c241748b1e90ce44a5757
Elly Pirelly <47147981+EllyPirelly@users.noreply.github.com>
2023-05-04 08:31:12 +0200

    add basic test construct for show/hide test

--- src/features/showHideAnEventsDetails.feature
+++ src/features/showHideAnEventsDetails.feature
@@ -1,18 +1,18 @@
-Feature: Show/Hide an events’ details
+Feature: Toggle between showing or hiding details of one event
 
-Scenario 1: An event element is collapsed by default.
+Scenario: An element is collapsed by default.
 Given the main page has been opened
-And the list of events has been rendered
-When a user hasn’t clicked the toggle button
+And a list of events has been rendered
+When the user has not clicked the toggle button
 Then the event element should be collapsed.
 
-Scenario 2: A user can expand an event element to see its details.
+Scenario: A user can expand an event element to see its details.
 Given the main page has been opened
-And the list of events has been rendered
-When a user clicks the toggle button
+And a list of events has been rendered
+When the user clicks the toggle button
 Then the event element should expand and show the event element details.
 
-Scenario 3: A user can collapse an event element to hide its details.
+Scenario: A user can collapse an event to hide its details.
 Given an event element is expanded and has been showing event element details
-When a user clicks the toggle button
+When the user clicks the toggle button
 Then the expanded event element should collapse.
--- src/features/showHideAnEventsDetails.test.js
+++ src/features/showHideAnEventsDetails.test.js
@@ -1,7 +1,56 @@
-// loadFeature loads a Gherkin file
-// defineFeature defines the code for that file
 import { loadFeature, defineFeature } from 'jest-cucumber';
 
 const feature = loadFeature('./src/features/showHideAnEventsDetails.feature');
 
-defineFeature(feature, test => { });
+defineFeature(feature, test => {
+  // test here
+  test('An element is collapsed by default.', ({ given, and, when, then }) => {
+    given('the main page has been opened', () => {
+
+    });
+
+    and('a list of events has been rendered', () => {
+
+    });
+
+    when('the user has not clicked the toggle button', () => {
+
+    });
+
+    then('the event element should be collapsed.', () => {
+
+    });
+  });
+
+  test('A user can expand an event element to see its details.', ({ given, and, when, then }) => {
+    given('the main page has been opened', () => {
+
+    });
+
+    and('a list of events has been rendered', () => {
+
+    });
+
+    when('the user clicks the toggle button', () => {
+
+    });
+
+    then('the event element should expand and show the event element details.', () => {
+
+    });
+  });
+
+  test('A user can collapse an event to hide its details.', ({ given, when, then }) => {
+    given('an event element is expanded and has been showing event element details', () => {
+
+    });
+
+    when('the user clicks the toggle button', () => {
+
+    });
+
+    then('the expanded event element should collapse.', () => {
+
+    });
+  });
+});
